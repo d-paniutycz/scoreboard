9acdfd9c615b9fb404d189e9f31bed991c0dcbfa
d-paniutycz <damian.paniutycz@gmail.com>
2023-10-31 13:05:26 +0100

    ci: workflow, phpunit and composer

---
+++ tst/WorkflowTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Paniutycz\Scoreboard\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkflowTest extends TestCase
+{
+    public function testAnyAssertion(): void
+    {
+        // assert
+        self::assertTrue(true);
+    }
+}
